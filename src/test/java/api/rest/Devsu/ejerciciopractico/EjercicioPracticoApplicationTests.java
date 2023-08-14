1c064c8bb136a0dfa2fb8c70316260ed49e3783a
Marco Salvador <salvador.systems@gmail.com>
2023-08-14 16:20:30 -0500

    JUnit 2

--- src/test/java/api/rest/Devsu/ejerciciopractico/EjercicioPracticoApplicationTests.java
+++ src/test/java/api/rest/Devsu/ejerciciopractico/EjercicioPracticoApplicationTests.java
@@ -2,33 +2,65 @@ package api.rest.Devsu.ejerciciopractico;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import api.rest.Devsu.ejerciciopractico.dto.ClienteDTO;
+import api.rest.Devsu.ejerciciopractico.dto.CuentaDTO;
 import api.rest.Devsu.ejerciciopractico.service.ClienteService;
+import api.rest.Devsu.ejerciciopractico.service.CuentaService;
 
 @SpringBootTest
 class EjercicioPracticoApplicationTests {
 
 	@Autowired
     private ClienteService clienteService;
+	@Autowired
+    private CuentaService cuentaService;
 	
 	@Test
 	void getClienteByIdenti() {
 		ClienteDTO clienteDTO = new ClienteDTO();
 		clienteDTO.setId(41L);
-		clienteDTO.setNombres("DANIELA ALEJANDRA LEIVA DEZA");
-		clienteDTO.setGenero("FEMENINA");
+		clienteDTO.setNombres("MARCO SALVADOR");
+		clienteDTO.setGenero("MASCULINO");
 		clienteDTO.setTipoDoc("DNI");
-		clienteDTO.setNumDoc("73415697");
+		clienteDTO.setNumDoc("72521448");
 		clienteDTO.setDireccion("CHIMBOTE-CAMINO REAL");
-		clienteDTO.setTelefono("923224324");
+		clienteDTO.setTelefono("931654207");
 		clienteDTO.setContraseña("123");
 		clienteDTO.setEstado("true");
-		ClienteDTO clienteDTOResult = clienteService.getClienteByIdenti("73415697");
+		ClienteDTO clienteDTOResult = clienteService.getClienteByIdenti("72521448");
 		assertEquals(clienteDTO.getId(), clienteDTOResult.getId());
+	}
+	
+	@Test
+	void getCuentaByCliente() {
+		
+		ClienteDTO clienteDTO = new ClienteDTO();
+		clienteDTO.setId(41L);
+		clienteDTO.setNombres("MARCO SALVADOR");
+		clienteDTO.setGenero("MASCULINO");
+		clienteDTO.setTipoDoc("DNI");
+		clienteDTO.setNumDoc("72521448");
+		clienteDTO.setDireccion("CHIMBOTE-CAMINO REAL");
+		clienteDTO.setTelefono("931654207");
+		clienteDTO.setContraseña("123");
+		clienteDTO.setEstado("true");
+		
+		CuentaDTO cuentaDTO = new CuentaDTO();
+		cuentaDTO.setId(14L);
+		cuentaDTO.setNumCuenta("987654321");
+		cuentaDTO.setTipCuenta("CUENTA AHORRO - VISA");
+		cuentaDTO.setSaldoInit(1580);
+		cuentaDTO.setEstado("true");
+		cuentaDTO.setClienteDTO(clienteDTO);
+		
+		CuentaDTO cuentaDTOResp = cuentaService.getCuenta(14L);
+		
+		assertEquals(cuentaDTOResp.getSaldoInit(), cuentaDTO.getSaldoInit());
 	}
 
 }
